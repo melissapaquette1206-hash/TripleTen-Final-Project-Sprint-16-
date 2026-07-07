a8c5c33d1fc8413c76c175b1e4639ff75529ec47
Melissa Paquette <melissapaquette1206@gmail.com>
2026-07-07 13:18:11 -0400

    fix errors based on feedback

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/TripleTen-Final-Project-Sprint-16-/",
   plugins: [react()],
 });
